c57fa424941916fb5235122edc0a1c102040b122
hadarrr <hadarkimel@gmail.com>
2022-11-15 18:01:56 +0200

    changed name file

--- HadarKimel.java
+++ HadarKimel.java
@@ -1,7 +1,7 @@
 public class HadarKimel{
 		public static void main(String[] args) {
 				for(int i=1;i<=5;i++){
-					System.out.println("Hadar Kimel "+i);
+					System.out.println("Hadar Kimel :) "+i);
 				}
 		}
 }
